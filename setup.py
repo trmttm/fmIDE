0b7aadbd02a9efd0b7926f3f1039edc8cd0112b7
trmttm <96355189+trmttm@users.noreply.github.com>
2021-12-27 22:53:22 +0400

    Add files via upload

---
+++ setup.py
@@ -0,0 +1,16 @@
+from setuptools import setup
+
+setup(
+    name='fmIDE',
+    version='',
+    packages=['Main', 'Tests', 'Tests', 'Pickles', 'Entities',
+              'Presenter', 'Resources', 'Utilities',
+              'ViewModel', 'Controller',
+              'Interactor', 'RequestModel', 'EntityGateway', 'ResponseModel', 'BoundaryOutput',
+              'ExternalSystems', 'PicklesCommands', 'Templates'],
+    url='',
+    license='',
+    author='STTM',
+    author_email='',
+    description=''
+)
