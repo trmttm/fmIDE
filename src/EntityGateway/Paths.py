c16e9077a4c4c10f5e12ccfcc9ddd7f819f9b693
yamaka <t.yamaka.1213@gmail.com>
2022-01-02 16:54:36 +0400

    Bug fix.

--- src/EntityGateway/Paths.py
+++ src/EntityGateway/Paths.py
@@ -13,10 +13,12 @@ def get_path(directory, file_name: str):
 def get_proper_path_depending_on_development_or_distribution(relative_path):
     possible_overlap = relative_path.split('/')[0]
     folder_path = os.path.join(sys.path[0], relative_path)
-    folder_path = remove_overlap(folder_path, possible_overlap)
+    if possible_overlap != '':
+        folder_path = remove_overlap(folder_path, possible_overlap)
     if not (os.path.exists(folder_path)):
         folder_path = f'{os.getcwd()}/{relative_path}'
-    folder_path = remove_overlap(folder_path, possible_overlap)
+    if possible_overlap != '':
+        folder_path = remove_overlap(folder_path, possible_overlap)
     return folder_path
 
 
