b8b6730cc8584b62337056e39585de8cdc4146ff
trmttm <96355189+trmttm@users.noreply.github.com>
2022-04-20 05:55:38 +0400

    Update GUI: Model CFWF

--- src/ExternalSystems/Configurations/test.py
+++ src/ExternalSystems/Configurations/test.py
@@ -44,54 +44,13 @@ class MyTestCase(unittest.TestCase):
 
             next_frame = switchable_frames[frame_number]
 
+            local_view_model = None
             if frame_number == 1:
-                frame1_frames = []
-                args = frame1_frames, view
-
-                view.clear_frame(next_frame)
-                local_stacker = Stacker(next_frame)
-                local_stacker.hstack(
-                    local_stacker.vstack(
-                        # Buttons
-                        *tuple(
-                            w.Button(f'frame{frame_number}_button_{n}').text(name).command(
-                                lambda i=n: switch_frames1(i, *args))
-                            for (n, name) in enumerate(names)
-                        ) + (w.Spacer(),)
-                    ),
-                    w.FrameSwitcher(f'frame{frame_number}_frame_switcher', local_stacker, frame1_frames).stackers(
-                        *tuple(
-                            frame1_each_page(local_stacker, names, name, view, w)
-                            for (i, name) in enumerate(names)
-                        ),
-                    ),
-                    w.Spacer().adjust(-1),
-                )
-                local_view_model = local_stacker.view_model
-                view.add_widgets(local_view_model)
+                local_view_model = create_frame_1_view_model(names, frame_number, next_frame, view)
             elif frame_number == 2:
-                frame2_frames = []
-                name = names[4]
-                number_of_products = int(view.get_value(get_entry_id(name)))
-                product_names = tuple(view.get_value(f'frame_1_entry_{name}_{n}') for n in range(number_of_products))
-                local_stacker = Stacker(next_frame)
-                local_stacker.hstack(
-                    local_stacker.vstack(
-                        *tuple(
-                            w.Button(f'frame_{frame_number}_{n}').text(product_name)
-                            for (n, product_name) in enumerate(product_names)
-                        ) + (w.Spacer(),)
-                    ),
-                    w.FrameSwitcher(f'frame{frame_number}_frame_switcher', local_stacker, frame2_frames).stackers(
-                        *tuple(
-                            frame2_each_page(local_stacker, name, view, w)
-                            for (i, name) in enumerate(product_names)
-                        ),
-                    ),
-                    w.Spacer().adjust(-1),
-                )
+                local_view_model = create_frame_2_view_model(names, frame_number, next_frame, view)
 
-                local_view_model = local_stacker.view_model
+            if local_view_model is not None:
                 view.add_widgets(local_view_model)
 
             view.switch_frame(next_frame)
@@ -172,21 +131,31 @@ def frame1_each_page(stacker_, names: tuple, name: str, view, widget):
     elif name == names[5]:
         return input_product_intercompany_sales(name, number, stacker_, w)
     else:
-        return stacker_.vstack_scrollable(
-            *tuple(
-                stacker_.vstack(
-                    stacker_.hstack(
-                        w.Label(f'{lbl(name, n, "")}').text(f'{name}{n} Name').width(20),
-                        w.Entry(get_entry_id2(name, n)).default_value(f'{name}{n}'),
-                    )) for n in range(number))
-        )
+        return stacker_.vstack_scrollable(*tuple(stacker_.vstack(
+            stacker_.hstack(
+                w.Label(f'{lbl(name, n, "")}').text(f'{name}{n} Name').width(20),
+                w.Entry(get_entry_id2(name, n)).default_value(f'{name}{n}'),
+            )
+        ) for n in range(number)))
 
 
 def lbl(name, n_, post_fix):
     return f'frame_1_label_{name}_{n_}_{post_fix}'
 
 
-def input_product(name, number, stacker_, w):
+def get_entry_id_product_fixed_cost(name, n) -> str:
+    return f'frame_1_entry_{name}_{n}_fixed_costs'
+
+
+def get_entry_id_product_variable_cost(name, n) -> str:
+    return f'frame_1_entry_{name}_{n}_variable_costs'
+
+
+def get_entry_id_product_inventory_cost(name, n) -> str:
+    return f'frame_1_entry_{name}_{n}_inventory_costs'
+
+
+def input_product(name, number_of_product, stacker_, w):
     return stacker_.vstack_scrollable(
         *tuple(
             stacker_.vstack(
@@ -196,17 +165,17 @@ def input_product(name, number, stacker_, w):
                 ),
                 stacker_.hstack(
                     w.Label(f'{lbl(name, n, "fixed_costs")}').text(f'n fixed costs').width(20).align('e'),
-                    w.Entry(f'frame_1_entry_{name}_{n}_fixed_costs').default_value(1),
+                    w.Entry(get_entry_id_product_fixed_cost(name, n)).default_value(1),
                 ),
                 stacker_.hstack(
                     w.Label(f'{lbl(name, n, "variable_costs")}').text(f'n variable costs').width(20).align('e'),
-                    w.Entry(f'frame_1_entry_{name}_{n}_variable_costs').default_value(1),
+                    w.Entry(get_entry_id_product_variable_cost(name, n)).default_value(1),
                 ),
                 stacker_.hstack(
                     w.Label(f'{lbl(name, n, "inventory_costs")}').text(f'n inventory costs').width(20).align('e'),
-                    w.Entry(f'frame_1_entry_{name}_{n}_inventory_costs').default_value(1),
+                    w.Entry(get_entry_id_product_inventory_cost(name, n)).default_value(1),
                 ),
-            ) for n in range(number))
+            ) for n in range(number_of_product))
     )
 
 
@@ -238,25 +207,107 @@ def input_product_intercompany_sales(name, number, stacker_, w):
     )
 
 
-def frame2_each_page(stacker_, name: str, view, widget):
+def frame2_each_page(stacker_, name, product_name: str, product_number: int, view, widget):
     w = widget
     frame_names = 'Fixed Cost', 'Variable Cost', 'Inventory'
-    return w.NoteBook(f'notebook_frame_2_{name}', stacker_).frame_names(frame_names).stackers(
-        stacker_.vstack(
-            w.Label(f'{frame_names[0]} {name}').text(f'{frame_names[0]} {name}'),
+    number_of_fixed_cost = int(view.get_value(get_entry_id_product_fixed_cost(name, product_number)))
+    number_of_variable_cost = int(view.get_value(get_entry_id_product_variable_cost(name, product_number)))
+    number_of_inventory_cost = int(view.get_value(get_entry_id_product_inventory_cost(name, product_number)))
+    return stacker_.vstack(
+        w.Label(f'frame_2_lable_{product_name}').text(product_name).padding(25, 0),
+        w.NoteBook(f'notebook_frame_2_{product_name}', stacker_).frame_names(frame_names).stackers(
+            stacker_.vstack(
+                *tuple(
+                    stacker_.hstack(
+                        w.Label(f'frame2_label_{product_name}_{n}_fixed_cost').text(f'Fixed Cost Name{n}'),
+                        w.Entry(f'frame2_entry_{product_name}_{n}_fixed_cost').default_value(f'Fixed Cost Name{n}'),
+                    ) for n in range(number_of_fixed_cost)),
+                w.Spacer(),
+            ),
+            stacker_.vstack(
+                *tuple(
+                    stacker_.hstack(
+                        w.Label(f'frame2_label_{product_name}_{n}_variable_cost').text(f'Variable Cost Name{n}'),
+                        w.Entry(f'frame2_entry_{product_name}_{n}_variable_cost').default_value(
+                            f'Variable Cost Name{n}'),
+                    ) for n in range(number_of_variable_cost)),
+                w.Spacer(),
+            ),
+            stacker_.vstack(
+                *tuple(
+                    stacker_.hstack(
+                        w.Label(f'frame2_label_{product_name}_{n}_inventory_cost').text(f'Inventory Cost Name{n}'),
+                        w.Entry(f'frame2_entry_{product_name}_{n}_inventory_cost').default_value(
+                            f'Inventory Cost Name{n}'),
+                    ) for n in range(number_of_inventory_cost)),
+                w.Spacer(),
+            ),
         ),
-        stacker_.vstack(
-            w.Label(f'{frame_names[1]} {name}').text(f'{frame_names[1]} {name}'),
-        ),
-        stacker_.vstack(
-            w.Label(f'{frame_names[2]} {name}').text(f'{frame_names[2]} {name}'),
-        ),
+        w.Spacer().adjust(-1),
     )
 
 
 def switch_frames1(n_: int, frame_names, view):
     next_frame = frame_names[n_]
     view.switch_frame(next_frame)
+
+
+def create_frame_1_view_model(names, frame_number, next_frame, view) -> list:
+    from stacker import Stacker
+    from stacker import widgets as w
+    frame1_frames = []
+    args = frame1_frames, view
+    view.clear_frame(next_frame)
+    local_stacker = Stacker(next_frame)
+    local_stacker.hstack(
+        local_stacker.vstack(
+            # Buttons
+            *tuple(
+                w.Button(f'frame{frame_number}_button_{n}').text(name).command(
+                    lambda i=n: switch_frames1(i, *args))
+                for (n, name) in enumerate(names)
+            ) + (w.Spacer(),)
+        ),
+        w.FrameSwitcher(f'frame{frame_number}_frame_switcher', local_stacker, frame1_frames).stackers(
+            *tuple(
+                frame1_each_page(local_stacker, names, name, view, w)
+                for (i, name) in enumerate(names)
+            ),
+        ),
+        w.Spacer().adjust(-1),
+    )
+    local_view_model = local_stacker.view_model
+    return local_view_model
+
+
+def create_frame_2_view_model(names, frame_number, next_frame, view) -> list:
+    from stacker import Stacker
+    from stacker import widgets as w
+
+    frame2_frames = []
+    name = names[4]
+    number_of_products = int(view.get_value(get_entry_id(name)))
+    product_names = tuple(view.get_value(f'frame_1_entry_{name}_{n}') for n in range(number_of_products))
+    local_stacker = Stacker(next_frame)
+    local_stacker.hstack(
+        # 1) Buttons
+        local_stacker.vstack(
+            *tuple(
+                w.Button(f'frame_{frame_number}_{n}').text(product_name)
+                for (n, product_name) in enumerate(product_names)
+            ) + (w.Spacer(),)
+        ),
+        # 2) Notebooks
+        w.FrameSwitcher(f'frame{frame_number}_frame_switcher', local_stacker, frame2_frames).stackers(
+            *tuple(
+                frame2_each_page(local_stacker, name, product_name, i, view, w)
+                for (i, product_name) in enumerate(product_names)
+            ),
+        ),
+        w.Spacer().adjust(-1),
+    )
+    local_view_model = local_stacker.view_model
+    return local_view_model
 
 
 if __name__ == '__main__':
